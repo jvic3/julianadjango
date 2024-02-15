fec23b42e1ce15b34e0749507358edfd2e1daf48
jvic3 <junemumo70@gmail.comgit commit --m initial>
2024-02-15 13:55:04 +0300

    initial commit

---
+++ julianadjango/urls.py
@@ -0,0 +1,12 @@
+
+from django.contrib import admin
+from django.urls import path
+from myapp import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',views.home),
+    path('gallery/',views.images),
+    path('about/',views.about),
+    path('contact/',views.contact),
+]
